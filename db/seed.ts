991bd8f60691ff4016003ac400717fc8c82fff8b
JT <Jack101091@gmail.com>
2024-03-21 12:26:34 -0400

    init astro db with user table and test usage

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,9 @@
-import { db } from 'astro:db';
+import { db, User } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+	await db.insert(User).values([
+		{userId: '123', email: 'jack101091@gmail.com'}
+	])
 }
